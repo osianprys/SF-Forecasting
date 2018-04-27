a381aeb5c86cc652ade2773868796b111bc27258
niklz <nhowlett87@gmail.com>
2018-04-27 14:42:57 +0100

    refactored shiny code

--- shinyApp/forecasting-dashboard/server.R
+++ shinyApp/forecasting-dashboard/server.R
@@ -17,73 +17,63 @@ load(file = "nBasedata.RData")
 
 # Define server logic required to draw a histogram
 function(input, output) {
+  
+ runFcasts <- reactive({
+   data <- switch(input$fcastMetric,
+                  SPC = SPC_df,
+                  nBase = nBase_df)
+   
+   metric <- switch(input$fcastMetric,
+                    SPC = "spc",
+                    nBase = "customers")
+   
+   
+   ts <- data %>%
+     filter(base == input$cbase) %>%
+     select(metric) %>%
+     map(~ts(.x,
+             start = c(year(min(input$dateRange[1])), month(max(input$dateRange[2]))),
+             frequency = 12))
+   
+   
+   
+   fit <- auto.arima(ts[[1]])
+   fcast <- forecast(fit, h = input$horizon)
+   
+   tday_values <- tail(fcast$x, 1) %>% as.data.frame()
+   
+   
+   fcast_values <- tail(fcast$mean, 1) %>% as.data.frame()
+   
+   fcast_upper <- tail(fcast$upper, 1) %>% as.data.frame()
+   
+   df <- cbind(tday_values$x[1], fcast_values$x[1], fcast_upper$`95%`[1]) %>%
+     as.data.frame()
+   names(df) <- c("Today", input$horizon, "Upper")
+   df <- df %>%
+     mutate(Var95 = Upper - Today) %>%
+     select(-Upper)
+   
+  df <-  round(df, 2)  
+  
+  out <- list(fcast = fcast,
+              df = df)
+   
+ })
+  
+  
+  
+  
    
   output$fcastPlot <- renderPlot({
     
-
-    
-  data <- switch(input$fcastMetric,
-                 SPC = SPC_df,
-                 nBase = nBase_df)
-  
-  metric <- switch(input$fcastMetric,
-                   SPC = "spc",
-                   nBase = "customers")
-  
-  
-    ts <- data %>%
-    filter(base == input$cbase) %>%
-    select(metric) %>%
-    map(~ts(.x,
-               start = c(year(min(input$dateRange[1])), month(max(input$dateRange[2]))),
-               frequency = 12))
-  
-  
-  
-    fit <- auto.arima(ts[[1]])
-    fcast <- forecast(fit, h = input$horizon)
-    autoplot(fcast)
+    autoplot(runFcasts()$fcast)
 
   })
   
   output$metrictable <- renderDataTable({
-
-    data <- switch(input$fcastMetric,
-                   SPC = SPC_df,
-                   nBase = nBase_df)
-    
-    metric <- switch(input$fcastMetric,
-                     SPC = "spc",
-                     nBase = "customers")
-    
-    
-    ts <- data %>%
-      filter(base == input$cbase) %>%
-      select(metric) %>%
-      map(~ts(.x,
-              start = c(year(min(input$dateRange[1])), month(max(input$dateRange[2]))),
-              frequency = 12))
-    
-    
-    
-    fit <- auto.arima(ts[[1]])
-    fcast <- forecast(fit, h = input$horizon)
-    
-      tday_values <- tail(fcast$x, 1) %>% as.data.frame()
-    
-    
-      fcast_values <- tail(fcast$mean, 1) %>% as.data.frame()
-    
-      fcast_upper <- tail(fcast$upper, 1) %>% as.data.frame()
-      
-      df <- cbind(tday_values$x[1], fcast_values$x[1], fcast_upper$`95%`[1]) %>%
-            as.data.frame()
-      names(df) <- c("Today", input$horizon, "Upper")
-      df <- df %>%
-            mutate(Var95 = Upper - Today) %>%
-            select(-Upper)
-    
-     round(df, 2)   
+  
+    runFcasts()$df
   })
   
 }
